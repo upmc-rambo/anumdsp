ab2438945d1b49811318d215d894d9cf9ffa9452
RamboJHB <jhbwdq@gmail.com>
2018-03-28 13:15:53 +0200

    sth

--- fir/tp3/main.c
+++ fir/tp3/main.c
@@ -1,109 +1,112 @@
+//
+//  main.c
+//  dsp_fir
+//
+//  Created by Jiang hongbo on 21/02/2018.
+//  Copyright © 2018 Jiang Hongbo. All rights reserved.
+//
+
 #include <stdio.h>
 #include "change_to_binary.h"
 #include "mult.h"
 #include "fulladder.h"
 
+#define B 4
 
-int main(int argc, char** argv){
+static unsigned char A[100][64];//MSB on the right
+static unsigned char X[100][64];
+static unsigned char Y[100][128];
 
-                   int i, j, a, b, c, d, e, f, g, h, out, N=12;
-                   unsigned char H[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char G[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char F[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char E[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char D[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char S[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char A[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char B[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char C[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char A0[12] = { 1,0,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char A1[12] = { 0,1,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char A2[12] = { 1,1,0,0,0,0,0,0,0,0,0,0};
-                   unsigned char Y[12] = { 0,0,0,0,0,0,0,0,0,0,0,0};
-                   
-                   printf("RIF:\n");
-                   printf("ao = 1, a1 = 2, a3 = 3\n");                   
-                   printf("tape 3 enter values:\n");
-                   printf("x[0]=");
-                   scanf("%d",&a);
-                   printf("x[1]=");
-                   scanf("%d",&b);
-                   printf("x[2]=");
-                   scanf("%d",&c);
-                   
-                   change_to_binary(a,A,N);
-                   change_to_binary(b,B,N);
-                   change_to_binary(c,C,N);
-                   			   				   
-		           /*for (i = N-1 ; i >= 0; i--){
-			             printf("tab_A[%d]=%d ",i , A[i]);
-		           }
-		           printf("\n");		           		         
-		           
-		           for (i = N-1 ; i >= 0; i--){
-			             printf("tab_B[%d]=%d ",i , B[i]);
-		           }
-		           printf("\n");
-		           
-		           for (i = N-1 ; i >= 0; i--){
-			             printf("tab_C[%d]=%d ",i , C[i]);
-		           }
-		           printf("\n");*/
-		           		        
-		           printf("\n");printf("\n");
-		           
-		           //calcul de Y[0]
-		           Y[0] = mult(A, 6, A0, 6);
-		           printf("Y[0]=%d ",Y[0]);
-		           
-		           //calcul de Y[1]
-		           d = mult(A, 6, A1, 6);
-		           change_to_binary(d,D,N);
-		           e = mult(B, 6, A0, 6);
-		           change_to_binary(e,E,N);
-		           Y[1] = NBITS_ADDER (D, E, 0, S, out, N);
-		           printf("Y[1]=%d ",Y[1]);
-		           
-		           //calcul de Y[2]
-		           d = mult(A, 6, A2, 6);
-		           change_to_binary(d,D,N);
-		           e = mult(B, 6, A1, 6);
-		           change_to_binary(e,E,N);
-		           f = mult(C, 6, A0, 6);
-		           change_to_binary(f,F,N);
-		           d = NBITS_ADDER (D, E, 0, S, out, N);
-		           change_to_binary(d,D,N);
-		           Y[2] = NBITS_ADDER (D, F, 0, S, out, N);
-		           printf("Y[2]=%d ",Y[2]);
-		           
-		           //calcul de Y[3]
-		           g = mult(B, 6, A2, 6);
-		           change_to_binary(g,G,N);
-		           h = mult(C, 6, A1, 6);
-		           change_to_binary(h,H,N);
-		           Y[3] = NBITS_ADDER (G, H, 0, S, out, N);
-		           printf("Y[3]=%d ",Y[3]);
-		           
-		           //calcul de Y[4]
-		           Y[4] = mult(C, 6, A2, 6);
-		           printf("Y[4]=%d ",Y[4]);
-		           printf("Y[5]=%d ",Y[5]);
-		           printf("Y[5]=%d ",Y[5]);		           
-		          
+/*  calculate y[n]
+ *  y[n] = x[0]*a[n] + x[1]*a[n-1] + ... + x[n]* a [0]
+ */
+
+void fir_filt(unsigned char** x,int x_len, unsigned char** y,int y_len ,int Bit){
+    
+    int i,yn;
+    int cin = 0, cout = 0;
+    unsigned char RES[100]={0};
+    memset(RES, 0, sizeof(unsigned char)*100);
+    unsigned char trans[100][100];
+    
+    printf("**********   OUTPUT  Y  ***********\n");
+    
+    for(yn=0;yn<y_len;yn++){
+        memset(trans, 0, sizeof(unsigned char)*10000);//reset
+        
+        for(i=0;i<=yn;i++){
+            mult((unsigned char*)A[i],Bit,(unsigned char*)X[yn-i],Bit,(unsigned char*)trans[i]);//---------------------trans[i]=a[i]*x[n-i]
+        }
+        
+        for (i = 0; i<=yn;i++)
+            NBITS_ADDER((unsigned char*)Y[yn], (unsigned char*)&trans[i],  cin, (unsigned char*)Y[yn],  cout, 2*Bit);//OUT+=trans[n];
+        
+        printf("Y[%d]=",yn);
+        print_binary_tab(Y[yn], 2*Bit-1);
+        
+    }
+    
 }
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+int main(int argc, const char * argv[]) {
+    int i,l,b;
+    
+    printf("RIF:\n");
+    printf("Enter Bits of X : ");
+    scanf("%d",&b);
+    printf("Enter Length of X : ");
+    scanf("%d",&l);
+    
+    
+    printf("**********ENTER VALUE X:***********\n");
+    int x[l];
+    for (i=0;i<l;i++){
+        printf("x[%d]=",i);
+        scanf("%d",&x[i]);
+        change_to_binary(x[i], X[i], b);
+    }
+    for (i=l;i<l+2;i++){
+        change_to_binary(0, X[i], b);
+    }
+    //******check enter value *******//
+    /*printf("X[%d][%d]=\n",l,b);
+     for (i=0;i<l+4;i++){
+     print_binary_tab(X[i], b);
+     }*/
+    printf("**********ENTER VALUE A:***********\n");
+    int a[3];
+    for (i=0;i<3;i++){
+        printf("a[%d]=",i);
+        scanf("%d",&a[i]);
+        change_to_binary(a[i], A[i], b);
+    }
+    for (i=3;i<l+4;i++){
+        change_to_binary(0, A[i], b);
+    }
+    //******check enter value *******//
+    /*printf("A[%d][%d]=\n",l,b);
+     for (i=0;i<l+4;i++){
+     print_binary_tab(A[i], b);
+     }*/
+    
+    fir_filt((unsigned char**)X ,l, (unsigned char**)Y,l+2, b);
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
